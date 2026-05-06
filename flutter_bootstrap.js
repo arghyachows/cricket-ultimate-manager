0fe256dbffca12c092a9796bfe32a37c1ea40c42
arghyachows <arghyachowdhury2610@gmail.com>
2026-05-06 17:45:40 +0000

    Deploying to gh-pages from @ arghyachows/cricket-ultimate-manager@fc47dbe39c78ab8ca9c07e1182874c2acc6ece9d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3816816567"
+    serviceWorkerVersion: "4092909374"
   }
 });
